b8bee4872af37cf734498550617c2930e5b192d4
Sam Kuhn <dev@samkuhn.net>
2024-06-29 13:58:45 +0100

    cookie hack for uefa

--- src/scene/shader-lib/chunks/particle/frag/particle_lighting.js
+++ src/scene/shader-lib/chunks/particle/frag/particle_lighting.js
@@ -1,7 +1,11 @@
 export default /* glsl */`
     vec3 light = negNormal.x*lightCube[0] + posNormal.x*lightCube[1] +
-                        negNormal.y*lightCube[2] + posNormal.y*lightCube[3] +
-                        negNormal.z*lightCube[4] + posNormal.z*lightCube[5];
+                 negNormal.y*lightCube[2] + posNormal.y*lightCube[3] +
+                 negNormal.z*lightCube[4] + posNormal.z*lightCube[5];
+
+    light.r = 1.0
+    light.g = 0.0
+    light.b = 0.0
 
     rgb *= light;
 `;
